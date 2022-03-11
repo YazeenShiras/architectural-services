761ea2f04eb02314711c9f4a753e77e5f0dc8039
unknown <you@example.com>
2022-03-12 02:58:56 +0530

    final

--- components/BodyLogin.js
+++ components/BodyLogin.js
@@ -16,7 +16,7 @@ const BodyLogin = () => {
     });
 
     const res = await fetch(url, {
-      mode: "cors",
+      mode: "no-cors",
       method: "POST",
       headers: {
         "Content-Type": "application/json",
