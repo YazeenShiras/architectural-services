3c23cbe9fca3caf2afcc0dff68c388d7671a0729
unknown <you@example.com>
2022-05-24 15:11:04 +0530

    gtag

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import Hero from "../components/Hero";
+import Script from "next/script";
 
 export default function Home() {
   return (
